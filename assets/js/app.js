72e63e7f6b934893b18d01e95603a2b9c7afa823
Thomas Johnson <tjohnson1106@gmail.com>
2018-06-04 13:06:59 -0400

    created js channel join keypress event and passed payload to channel logger message to user

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,3 +24,31 @@ let channel = socket.channel("chat_room:lobby", {});
 let list = $("#message-list");
 let message = $("#msg");
 let name = $("#name");
+
+message.on("keypress", event => {
+  if (event.keyCode == 13) {
+    channel.push("shout", {
+      name: name.val(),
+      message: message.val()
+    });
+    message.val("");
+  }
+});
+
+channel.on("shout", payload => {
+  list.append(
+    `<b>${payload.name || "new_user"}:</b> ${payload.message}<br>`
+  );
+  list.prop({
+    scrollTop: list.prop("scrollHeight")
+  });
+});
+
+channel
+  .join()
+  .receive("ok", resp => {
+    console.log("joined successfully", resp);
+  })
+  .receive("error", resp => {
+    console.log("unable to join", resp);
+  });
